8270a4c90c8ecc051cc3eb9899bc83768ad6cf45
bfrerot <benoit.frerot.uk@gmail.com>
2025-04-02 17:44:54 +0200

    02042025

--- Logical-Bitwise.py
+++ Logical-Bitwise.py
@@ -78,3 +78,51 @@ print(a >> 1)  # 2 (0010 in binary)
 # a = 5 0101
 # (a >> 1) = 0010 = 2
 # (a >> 2) = 0001 = 1
+
+
+########## Deal with SINGLE bits
+
+# Pour comprendre..
+
+# exemple de variable sur laquelle on doit prendre 1 bit en considération pour notre tache/travail/etc
+flag_register = 0x1234
+# en binaire = 0000 0000 0000 0000 0001 0010 0011 0100 
+# ordre des bits 31, 30, etc, 0, de gauche à droite
+# on nous attribue le 3ème bit
+# flag_register = 0000000000000000000000000000x000
+
+### Checker l'état du bit
+# on peut utiliser cette conjunction property:
+# x & 1 = x
+# x & 0 = 0
+# on applique un bit mask au bit que l'on souhaite tester
+# 00000000000000000000000000001000
+# 0000000000000000000000000000x000
+# si le résultat est True égal à 1 c'est que le bit esc activé
+# sinon, le bit est False, égal à 0, désactivé
+
+the_mask = 8 # = 0000 0000 0000 0000 0000 0000 0000 1000
+if flag_register & the_mask:
+    print ("My bit is set")
+else:
+    print ("My bit is NOT set")
+
+
+### Reseter l'état du bit
+flag_register = flag_register & ~the_mask
+# ~the_mask = 1111 1111 1111 1111 1111 1111 1111 0111
+flag_register &= ~the_mask
+
+
+### Set le bit à 1
+# x | 1 = 1
+# x | 0 = x
+flag_register = flag_register | the_mask
+flag_register |= the_mask
+
+
+### Inverser le bit
+# x ^ 1 = ~x
+# x ^ 0 = x
+flag_register = flag_register ^ the_mask
+flag_register ^= the_mask
--- Loops.py
+++
@@ -1,7 +0,0 @@
-# modifier un string - rajouter un espace entre chaque caractere
-
-exampleString = 'silly walks'
-for ix in range(len(exampleString)):  #iteration
-  print(exampleString[ix], end=' ')
-s i l l y   w a l k s
-
