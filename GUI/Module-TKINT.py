27a7be6fcdc8aa479ee531c99b84968d77a27d6e
bfrerot <benoitfrerot13@gmail.com>
2025-10-03 18:10:29 +0200

    03102025

--- GUI/Module-TKINT.py
+++ GUI/Module-TKINT.py
@@ -70,3 +70,94 @@ skylight.mainloop()
 
 
 
+### settling widgets
+
+# 3*methods
+#   - place() => forces to precisely declare a widget's location, pixel by pixel
+#                however, doesn't protect from common mistakes causing widgets to overlap each other or to place some of them, partially or fully, outside the window
+#   - pack() => tries to guess the developer's intentions and to find the best location for each widget
+#   - grid() => permits to express general wishes and tries to deploy the widgets according to them
+
+
+## place()
+
+# height=h = the widget's desired height measured in pixels; if the parameter is omitted, the widget's height will be determined automatically
+# width=w  = the widget's desired width measured in pixels; if the parameter is omitted, the widget's width will be determined automatically
+# x=x      = the widget's top-left pixel's horizontal coordinate measured relative to the home window's top-left corner
+# y=y      = the widget's top-left pixel's vertical coordinate measured relative to the home window's top-left corner
+
+import tkinter as tk
+
+window = tk.Tk()
+button_1 = tk.Button(window, text="Button #1")
+button_2 = tk.Button(window, text="Button #2")
+button_3 = tk.Button(window, text="Button #3")
+button_1.place(x=10, y=10)   # x from left to right, y from up to down
+button_2.place(x=20, y=40)
+button_3.place(x=30, y=90)
+window.mainloop()
+
+
+## grid()
+
+# column=c        = deploys the widget in the column number c; start from zero, and if we omit this argument, the manager will assume 0 (the left-most column)
+# row=r           = deploys the widget in the row number r; if we omit this argument, the manager will assume the first free row starting from the top
+# columnspan=cs   = determines how many neighboring columns the widget occupies; the parameter defaults to 1 (the widget won't cross a single grid's cell)
+# rowspan=rs      = works as columnspan but refers to rows
+
+import tkinter as tk
+
+window = tk.Tk()
+button_1 = tk.Button(window, text="Button #1")
+button_2 = tk.Button(window, text="Button #2")
+button_3 = tk.Button(window, text="Button #3")
+button_1.grid(row=0, column=0)
+button_2.grid(row=1, column=1)
+button_3.grid(row=2, column=2)
+window.mainloop()
+
+# if a duplicate, the last is on the top
+button_2.grid(row=1, column=0)
+button_3.grid(row=1, column=0)  # ==> this one will appear
+
+# we cannot jump columns
+button_1.grid(row=0, column=0)
+button_2.grid(row=1, column=1)
+button_3.grid(row=2, column=5) # => remains on column 2
+
+# to play with placement, with columnspan
+button_1.grid(row=0, column=0)
+button_2.grid(row=1, column=1)
+button_3.grid(row=2, column=0, columnspan=2) # manager will consider the window has 2 columns
+
+
+## pack()
+
+# side = s – forces the manager to pack the widgets in a specified direction, where s can be specified as:
+#       TOP – the widget is packed toward the window's top = default
+#       BOTTOM – the widget is packed toward the window's bottom
+#       LEFT – toward the window's left boundary
+#       RIGHT – toward the window's right boundary
+
+# fill = f – suggests to the manager how to expand the widget if you want it to occupy more space than the default, while f should be specified as:
+#        NONE – do not expand the widget = default
+#        X – expand it in the horizontal direction
+#        Y – expand it in the vertical direction
+#        BOTH – expand it in both directions
+
+import tkinter as tk
+
+window = tk.Tk()
+button_1 = tk.Button(window, text="Button #1")
+button_2 = tk.Button(window, text="Button #2")
+button_3 = tk.Button(window, text="Button #3")
+button_1.pack()
+button_2.pack()
+button_3.pack()
+window.mainloop()
+
+# use of side
+button_1.pack(side=tk.RIGHT)
+button_2.pack()
+button_3.pack()
+
--- SandBox.py
+++ SandBox.py
@@ -1,14 +1,10 @@
-import tkinter
-from tkinter import messagebox
+import tkinter as tk
 
-def Click():
-    replay = messagebox.askquestion("Quit?", "Are you sure?")
-    if replay == 'yes':
-        skylight.destroy();
-
-
-skylight = tkinter.Tk()
-skylight.title("Skylight")
-button = tkinter.Button(skylight, text="Bye!", command=Click)
-button.place(x=10, y=10)
-skylight.mainloop()
+window = tk.Tk()
+button_1 = tk.Button(window, text="Button #1")
+button_2 = tk.Button(window, text="Button #2")
+button_3 = tk.Button(window, text="Button #3")
+button_1.pack(side=tk.RIGHT, fill=tk.Y)
+button_2.pack()
+button_3.pack()
+window.mainloop()
